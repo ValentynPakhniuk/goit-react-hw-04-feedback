6ade56d6f8d59e0c6ba8e5631dfdfcd8a497caae
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-20 15:10:22 +0300

    create Statistics.jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,36 @@
+import { Notification } from '../Notification/Notification';
+import PropTypes from 'prop-types';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <div>
+      {total === 0 ? (
+        <div>
+          <Notification message="There is no feedback"></Notification>
+        </div>
+      ) : (
+        <div>
+          <p>Goog: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {total}</p>
+          <p>Positive feedback: {positivePercentage}%</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
