de1a36beff68b01276e52603576e7bf47942cddf
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-20 15:08:00 +0300

    update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { useState } from 'react';
+import { Container } from './Container.styled';
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = { good, neutral, bad };
+
+  const handelClick = e => {
+    switch (e.target.id) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+
+      default:
+        console.error('Undefined option');
+    }
+  };
+
+  const total = good + neutral + bad;
+  const positivePercentage = Math.round((good / (good + neutral + bad)) * 100);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(options)}
+          handelClick={handelClick}
+        ></FeedbackOptions>
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        ></Statistics>
+      </Section>
+    </Container>
   );
 };
